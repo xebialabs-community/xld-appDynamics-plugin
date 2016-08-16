d680b9a4040e849257661542f25eb844fb1ce400
Vincent Lussenburg <vlussenburg@xebia.com>
2016-08-15 23:17:10 -0600

    working version

--- src/main/resources/appdynamics/installAppDynamicsAgent.sh
+++ src/main/resources/appdynamics/installAppDynamicsAgent.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 
 if [[ -d "${container.appDynamicsHome}" ]]; then
 	echo "AppDynamics homedir ${container.appDynamicsHome} already exists, skipping unzip..."
@@ -9,21 +8,45 @@ else
 	unzip AppServerAgent.zip -d "${container.appDynamicsHome}"	
 fi
 
-if [[ ! -d "${container.appDynamicsHome}" ]]; then
+echo "Copying controller-info to conf dir.."
+cp controller-info.xml "${container.appDynamicsHome}/AppServerAgent/conf/"
+
+if [[ ! -f "${container.appDynamicsHome}/AppServerAgent/conf/controller-info.xml" ]]; then
+	echo "Install failed.. check error messages."
 	exit 1
 fi
 
-echo "Copying controller-info to conf dir.."
-cp controller-info.xml "${container.appDynamicsHome}/conf"
+if [[ -f "${container.startupScript}.xld-bak" ]]; then
+	echo "AppDynamics agent already installed before - resetting startup script to backupped version..."
+	rm -f ${container.startupScript}
+	cp "${container.startupScript}.xld-bak" "${container.startupScript}"
+else
+	echo "Backupping script to ${container.startupScript}.xld-bak before modification..."
+	cp "${container.startupScript}" "${container.startupScript}.xld-bak"
+fi
 
-echo "Modifying and backupping startup script.."
-sed -e '1 a ### START XL DEPLOY ADDITION ###' \
-	-e '1 a JAVA_OPTS=$JAVA_OPTS:-javaagent:${container.appDynamicsHome}/javaagent.jar' \
-    -e '1 a ### END XL DEPLOY ADDITION ###' \
-    ${container.startupScript} > out.file
+JAVA_OPTS="-javaagent:${container.appDynamicsHome}/javaagent.jar"
 
-mv ${container.startupScript} ${container.startupScript}.xld-bak
-mv out.file ${container.startupScript}
+<#if container.applicationName??>
+	JAVA_OPTS="$JAVA_OPTS -Dappdynamics.agent.applicationName=${container.applicationName}"
+</#if>
 
-# ALWAYS fail for now.. work in progress
-exit 1
+<#if container.tierName??>
+	JAVA_OPTS="$JAVA_OPTS -Dappdynamics.agent.tierName=${container.tierName}"
+</#if>
+
+<#if container.nodeName??>
+	JAVA_OPTS="$JAVA_OPTS -Dappdynamics.agent.nodeName=${container.nodeName}"
+</#if>
+
+echo "Modifying startup script.."
+sed -e "1 a ### START XL DEPLOY ADDITION ###" \
+	-e "1 a JAVA_OPTS=\$JAVA_OPTS $JAVA_OPTS" \
+	-e "1 a ### END XL DEPLOY ADDITION ###" \
+	"${container.startupScript}" > out.file \
+	&& mv out.file "${container.startupScript}"
+EXITCODE=$?
+
+echo "AppDynamics agent added to startup scripts. Restart JVM for it to take effect."
+
+exit $EXITCODE
